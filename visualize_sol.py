0d70b21335c4fad2575d27889bfbd7d148b8d2db
ryo_kurosu <dtrjs.kurosu.0510@gmail.com>
2023-01-20 09:39:52 +0900

    fix test.py to def main()

--- visualize_sol.py
+++ visualize_sol.py
@@ -2,6 +2,12 @@ import pandas as pd
 import sys
 import openpyxl as excel
 
+# color setting
+red_fill = excel.styles.PatternFill(patternType='solid', fgColor='FF0000')
+blue_fill = excel.styles.PatternFill(patternType='solid', fgColor='0000FF')
+green_fill = excel.styles.PatternFill(patternType='solid', fgColor='00FF00')
+
+
 def main():
     a = 4 # number of row of block
     b = 8 # number of column of block
@@ -11,21 +17,21 @@ def main():
     
     ws = wb.active
     
-    red = excel.styles.PatternFill(patternType='solid', fgColor='FF0000')
-    blue = excel.styles.PatternFill(patternType='solid', fgColor='0000FF')
-    green = excel.styles.PatternFill(patternType='solid', fgColor='00FF00')
-    
     for i in range(a):
         for j in range(b):
-            ws.cell(row=2*i+1, column=2*j+1).value = i*a+j
-            ws.cell(row=2*i+1, column=2*j+1).fill = red
+            if (i*a+j) % 3 == 0:
+                ws.cell(row=2*i+1, column=2*j+1).value = i*a+j
+                ws.cell(row=2*i+1, column=2*j+1).fill = red_fill
+            elif (i*a+j) % 3 == 1:
+                ws.cell(row=2*i+1, column=2*j+1).value = i*a+j
+                ws.cell(row=2*i+1, column=2*j+1).fill = blue_fill
+            else:
+                ws.cell(row=2*i+1, column=2*j+1).value = i*a+j
+                ws.cell(row=2*i+1, column=2*j+1).fill = green_fill
+                
             
     wb.save("results_sample.xlsx")
     
     return
 
 main()
-    
-    
-    
-    
